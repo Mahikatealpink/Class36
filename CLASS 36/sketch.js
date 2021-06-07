ff6f01bc1c1ef90bdeda8f74f4c862ce31923ad5
Mahikatealpink <76555315+Mahikatealpink@users.noreply.github.com>
2021-06-07 15:59:42 +0530

    Add files via upload

---
+++ CLASS 36/sketch.js
@@ -0,0 +1,12 @@
+var canvas,gameState=0;
+var playerCount,database,form,player,game;
+
+function setup(){
+  canvas=createCanvas(400,400)
+  database=firebase.database()
+  game.getState()
+  game.start()
+}
+function draw(){
+  
+}
